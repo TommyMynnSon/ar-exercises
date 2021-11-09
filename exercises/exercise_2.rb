800c82c75e8c53414051ccb1240b22400d068496
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-09 15:17:18 -0800

    Load store instances from psql with id 1, 2 and update store instance with id 1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Return the first store (with id = 1) from the database
+@store1 = Store.first
+
+# Return the second store from the database
+@store2 = Store.find_by(id: 2)
+
+# Update @store1 database instance by changing its name
+@store1.update(name: 'Kingsway')
