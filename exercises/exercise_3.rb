5862c19f89db0ef04dd54b5a088c2738fecae28e
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-09 15:21:20 -0800

    Load store instance with id 3 and destroy it, verify with count method

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the third store
+@store3 = Store.find_by(id: 3)
+
+# Delete @store3 from database using Active Record's destroy method
+@store3.destroy
+
+# Verify deletion by outputting number of store instances in stores table
+puts Store.count
