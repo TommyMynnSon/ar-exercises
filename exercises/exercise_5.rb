98cf08ef2202e03ecfad04f66326ec0f5db990fd
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-11-09 16:51:18 -0800

    Perform calculation methods on stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# Output total revenue for all stores using
+puts "Aggregate Revenue: $#{Store.sum(:annual_revenue)}"
+
+# Output average annual revenue for all stores
+puts "Average Revenue: $#{Store.sum(:annual_revenue) / Store.count}"
+
+# Output number of stores that are generating $1M or more in annual sales
+puts "Number of stores w/ $1M or more in annual revenue: #{Store.where('annual_revenue >= 1000000').size}"
